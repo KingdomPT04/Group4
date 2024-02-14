ed89c642392367d1844963bfed9c5ba5fae26f8c
KingdomPT04 <160001469+KingdomPT04@users.noreply.github.com>
2024-02-14 08:00:57 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -493,7 +493,7 @@ function cartFunctionalities(addItem, trimedEmailID, addToCartBtn) {
                 if (item.FoodID === id && item.Quantity >= 1) {
                     item.Quantity -= 1;
                     if (item.Quantity === 0) {
-                        if (window.location != 'https://eddy3018.github.io/Group4/user-orders.html') {
+                        if (window.location != 'https://kingdompt04.github.io/Group4/user-orders.html') {
                             // Enable Buttons - so user can use them again
                             // Enable removed items btn
                             addToCartBtn[item.FoodID - 1].disabled = false;
@@ -529,7 +529,7 @@ function clearUserCart(addItem, addToCartBtn, trimedEmailID) {
     cartItemsContainer.innerHTML = '';
     cartValues.forEach(values => { values.innerHTML = '0'; })
     cartTotal.innerHTML = '0';
-    if (window.location != 'https://eddy3018.github.io/Group4/user-orders.html') {
+    if (window.location != 'https://kingdompt04.github.io/Group4/user-orders.html') {
         // Enable removed items btn
         addItem.forEach(item => {
             addToCartBtn[item.FoodID - 1].disabled = false;
@@ -606,7 +606,7 @@ function ClientDataFlow(addToCartBtn) {
                             // Store previouly added items to array -
                             addItem.push(userCart[i])
                                 // Disable already added items
-                            if (window.location != 'https://eddy3018.github.io/Group4/user-orders.html') {
+                            if (window.location != 'https://kingdompt04.github.io/Group4/user-orders.html') {
                                 addToCartBtn[userCart[i].FoodID - 1].disabled = true;
                                 addToCartBtn[userCart[i].FoodID - 1].innerHTML = 'In Cart';
                             }
@@ -650,7 +650,7 @@ function ClientDataFlow(addToCartBtn) {
                         title: 'Order Successfully Recorded',
                     });
                     window.setTimeout(function() {
-                        window.location.replace('https://eddy3018.github.io/Group4/user-orders.html')
+                        window.location.replace('https://kingdompt04.github.io/Group4/user-orders.html')
                     }, 2600)
                 } else {
                     Swal.fire({
@@ -661,7 +661,7 @@ function ClientDataFlow(addToCartBtn) {
             })
 
             // Shows Orders
-            if (window.location.href === 'https://eddy3018.github.io/Group4/user-orders.html') {
+            if (window.location.href === 'https://kingdompt04.github.io/Group4/user-orders.html') {
                 setOrderDetails(trimedEmailID)
             }
         } else {
